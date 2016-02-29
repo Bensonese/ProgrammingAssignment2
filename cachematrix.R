cd9a4f2687cad5ba50175b8a126b94995f850c8b
Bensonese <bhsu@cochlear.com>
2016-02-29 01:48:18 +0100

    R Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -9,46 +9,67 @@
 ## non-matrices or non-square matrices
 
 makeCacheMatrix <- function(x = matrix()) {
-    if(!is.matrix(x)) {
-        stop("This function can only take a matrix as its argument!")
-    } else if (nrow(x) != ncol(x)) {
-        stop("Sorry, this function can only accept a SQUARE matrix.")
-    }
-    inv <- NULL
-    set <- function(y) {
-        if(!is.matrix(y)) {
-            stop("This function can only take a matrix as its argument!")
-        } else if (nrow(y) != ncol(y)) {
-            stop("Sorry, this function can only accept a SQUARE matrix.")
-        }
-        x <<- y
-        inv <<- NULL
-    }
-    get <- function() x
-    setinv <- function(b) {
-        if(!is.matrix(b)) {
-            stop("This function can only take a matrix as its argument!")
-        } else if (nrow(b) != ncol(b)) {
-            stop("Sorry, this function can only accept a SQUARE matrix.")
-        }
-        inv <<- b
-    }
-    getinv <- function() inv
-    list(set = set, get = get, setinv = setinv, getinv = getinv)
+     
+     if(!is.matrix(x)) {
+          stop("This function can only take a matrix as its argument!")
+     } else if (nrow(x) != ncol(x)) {
+          stop("Sorry, this function can only accept a SQUARE matrix.")
+     }
+     
+     inv <- NULL
+     set <- function(y) {
+          
+          if(!is.matrix(y)) {
+               stop("This function can only take a matrix as its argument!")
+          } else if (nrow(y) != ncol(y)) {
+               stop("Sorry, this function can only accept a SQUARE matrix.")
+          }
+          
+          x <<- y
+          inv <<- NULL
+          
+     }
+     
+     get <- function() x
+     
+     setinv <- function(b) {
+          
+          if(!is.matrix(b)) {
+               stop("This function can only take a matrix as its argument!")
+          } else if (nrow(b) != ncol(b)) {
+               stop("Sorry, this function can only accept a SQUARE matrix.")
+          }
+          
+          inv <<- b
+          
+     }
+     
+     getinv <- function() inv
+     
+     list(set = set, get = get, setinv = setinv, getinv = getinv)
+     
 }
 
 ## cacheSolve computes the inverse of the square matrix returned by
 ## makeCacheMatrix if none is already available from the cache
 
 cacheSolve <- function(x, ...) {
-    inv <- x$getinv()
-    if(!is.null(inv)) {
-        message("Cached data retrieved")
-        return(inv)
-    }
-    b <- x$get()
-    inv <- solve(b, ...)
-    x$setinv(inv)
-    message("New matrix inverse calculated")
-    inv     
+     
+     inv <- x$getinv()
+     
+     if(!is.null(inv)) {
+          message("Cached data retrieved")
+          return(inv)
+     }
+     
+     b <- x$get()
+     
+     inv <- solve(b, ...)
+     
+     x$setinv(inv)
+     
+     message("New matrix inverse calculated")
+     
+     inv
+     
 }
