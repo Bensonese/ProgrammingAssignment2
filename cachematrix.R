10879671a51b8edd405a97c48e4ff86513b8cce8
Bensonese <bhsu@cochlear.com>
2016-02-29 01:11:51 +0100

    R Assignment #2
    Final version for R Programming Assignment No. 2.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The makeCacheMatrix function stores an n-by-n (square) matrix in memory
+## as well as caches the inverse of this particular matrix if available.
+## The cacheSolve function computes and stores the inverse of the
+## square matrix returned by makeCacheMatrix if none is already available
+## in the memory.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a special "square matrix" object that can
+## cache its inverse and rejects all non-valid inputs such as
+## non-matrices or non-square matrices
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    if(!is.matrix(x)) {
+        stop("This function can only take a matrix as its argument!")
+    } else if (nrow(x) != ncol(x)) {
+        stop("Sorry, this function can only accept a SQUARE matrix.")
+    }
+    inv <- NULL
+    set <- function(y) {
+        if(!is.matrix(y)) {
+            stop("This function can only take a matrix as its argument!")
+        } else if (nrow(y) != ncol(y)) {
+            stop("Sorry, this function can only accept a SQUARE matrix.")
+        }
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinv <- function(b) {
+        if(!is.matrix(b)) {
+            stop("This function can only take a matrix as its argument!")
+        } else if (nrow(b) != ncol(b)) {
+            stop("Sorry, this function can only accept a SQUARE matrix.")
+        }
+        inv <<- b
+    }
+    getinv <- function() inv
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve computes the inverse of the square matrix returned by
+## makeCacheMatrix if none is already available from the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    if(!is.null(inv)) {
+        message("Cached data retrieved")
+        return(inv)
+    }
+    b <- x$get()
+    inv <- solve(b, ...)
+    x$setinv(inv)
+    message("New matrix inverse calculated")
+    inv     
 }
